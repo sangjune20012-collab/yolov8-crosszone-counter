c4b5e778737232b5d0e9c39f6862c576357be6f2
박상준 <sangjune20012@gmail.com>
2026-06-19 14:50:26 +0900

    Remove old repository contents

--- Extract_First_Frame.py
+++
@@ -1,9 +0,0 @@
-import supervision as sv
-import cv2
-
-generator = sv.get_video_frames_generator("./mall.mp4")
-iterator = iter(generator)
-frame = next(iterator)
-
-cv2.imwrite("frame.jpg", frame)
-
